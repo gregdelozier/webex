1e176e530efaa073bd8d89cafa2410a183af5129
Greg DeLozier <gregdelozier@users.noreply.github.com>
2026-04-20 18:25:18 +0000

    Added another route

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return "Hello, Dr. DeLozier"
+
+@app.route("/hello")
+def hello():
+    return "Hello, Everyone!"
 
 if __name__ == "__main__":
     app.run(debug=True)
